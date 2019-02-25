bd3303a126453fb85e63d900fc9f25137914d3ad
Ignis Deus <ignisdeus2048@gmail.com>
2019-02-25 11:30:01 +0700

    Multiplication done

---
+++ MatrixMultiplicationTask/Matrix.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiplicationTask
+{
+    public class Matrix
+    {
+        private readonly double[,] matrix;
+        public int Rows => matrix.GetLength(0);
+        public int Columns => matrix.GetLength(1);
+        public double this[int row, int col]
+        {
+            get { return matrix[row, col]; }
+            set { matrix[row, col] = value; }
+        }
+
+        public Matrix(double[,] matrix)
+        {
+            if (matrix == null)
+            {
+                throw new ArgumentNullException(nameof(matrix));
+            }
+            if (matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            {
+                throw new ArgumentException("Dimensions must be greater than zero", nameof(matrix));
+            }
+            this.matrix = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            CopyMatrix(matrix);
+        }
+
+        public Matrix(Matrix other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+            this.matrix = new double[other.Rows, other.Columns];
+            CopyMatrix(other.matrix);
+        }
+
+        public Matrix(int rows, int columns)
+        {
+            if (rows <= 0)
+            {
+                throw new ArgumentException("Must be greater than zero", nameof(rows));
+            }
+            if (columns <= 0)
+            {
+                throw new ArgumentException("Must be greater than zero", nameof(columns));
+            }
+            this.matrix = new double[rows, columns];
+        }
+
+        private void CopyMatrix(double[,] other)
+        {
+            for (int i = 0; i < other.GetLength(0); i++)
+            {
+                for (int j = 0; j < other.GetLength(1); j++)
+                {
+                    this.matrix[i, j] = other[i, j];
+                }
+            }
+        }
+
+        public Matrix Multiply(Matrix other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+            if (Columns != other.Rows)
+            {
+                throw new InvalidOperationException("Matrices are not consistent");
+            }
+            var result = new double[Rows, other.Columns];
+            Parallel.For(0, Rows, row =>
+            {
+                Parallel.For(0, other.Columns, col =>
+                {
+                    for (int i = 0; i < Columns; i++)
+                    {
+                        result[row, col] += matrix[row, i] * other.matrix[i, col];
+                    }
+                });
+            });
+            return new Matrix(result);
+        }
+    }
+}
---
+++ MatrixMultiplicationTask/MatrixTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MatrixMultiplicationTask
+{
+    [TestFixture]
+    public class MatrixTests
+    {
+        [Test]
+        public void IfNotConsistent_Fails()
+        {
+            var a = new Matrix(2, 2);
+            var b = new Matrix(3, 3);
+            Assert.Throws<InvalidOperationException>(() => a.Multiply(b));
+        }
+
+        [TestCaseSource("MultiplyTestSource")]
+        public void MultiplyTest(Matrix a, Matrix b, Matrix abExpect)
+        {
+            var ab = a.Multiply(b);
+            Assert.AreEqual(abExpect.Columns, ab.Columns);
+            Assert.AreEqual(abExpect.Rows, ab.Rows);
+            for (int i = 0; i < ab.Rows; i++)
+            {
+                for (int j = 0; j < ab.Columns; j++)
+                {
+                    Assert.AreEqual(abExpect[i, j], ab[i, j], 1e-5);
+                }
+            }
+        }
+
+        [TestCaseSource("LoadTestSource")]
+        public void LoadTest(int count, int size)
+        {
+            var a = new Matrix(size, size);
+            var b = new Matrix(size, size);
+            for (int i = 0; i < count; i++)
+            {
+                a.Multiply(b);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> MultiplyTestSource()
+        {
+            yield return new TestCaseData(
+                new Matrix(new double[,] { { 2, 3 }, { 6, 5 } }),
+                new Matrix(new double[,] { { 8, 9, 7 }, { 5, 3, 5 } }),
+                new Matrix(new double[,] { { 31, 27, 29 }, { 73, 69, 67 } }));
+            yield return new TestCaseData(
+                new Matrix(new double[,] { { 1 }, { 4 }, { 3 } }),
+                new Matrix(new double[,] { { 2, 4, 1 } }),
+                new Matrix(new double[,] { { 2, 4, 1 }, { 8, 16, 4 }, { 6, 12, 3 } }));
+            yield return new TestCaseData(
+                new Matrix(new double[,] { { 2, 4, 1 } }),
+                new Matrix(new double[,] { { 1 }, { 4 }, { 3 } }),
+                new Matrix(new double[,] { { 21 } }));
+        }
+
+        public static IEnumerable<TestCaseData> LoadTestSource()
+        {
+            yield return new TestCaseData(1, 5);
+            yield return new TestCaseData(20000, 2);
+            yield return new TestCaseData(1000, 10);
+            yield return new TestCaseData(50, 50);
+            yield return new TestCaseData(10, 200);
+        }
+    }
+}
